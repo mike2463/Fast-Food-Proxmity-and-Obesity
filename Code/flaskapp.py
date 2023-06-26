1e2a4ecc5c04eb4b5d267cdc8d0e7769c7b6d044
zach-hb <zachblack03@gmail.com>
2023-06-25 23:26:26 -0500

    restaurant data accessible in flask api

--- Code/flaskapp.py
+++ Code/flaskapp.py
@@ -34,20 +34,40 @@ session = Session(engine)
 # Query the data from the table
 results = session.query(coordinates).all()
 
-# Convert the results to a list of dictionaries
-data = []
-for result in results:
-    data.append(result.__dict__)
-
-# Close the session
-session.close()
-
 # Return the data as JSON
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return jsonify(data)
+def homepage():
+    """list available routes"""
+    return (
+        f"Available Routes: <br/>"
+        f"/api/v1.0/restaurant_data<br/>"
+        f"/api/v1.0/county_data<br/>"
+        f"/api/v1.0/state_data<br/>"
+        f"/api/v1.0/wic_obesity"        
+    )
+
+@app.route("/api/v1.0/restaurant_data")
+def rest_data():
+    """add query results into dict, then jsonify it."""
+    coords = session.query(coordinates).all()
+    rests = session.query(restaurant_names).all()
+    lat = []
+    long = []
+    rnames = []
+    for coord in coords:
+        lat.append(coord.lat)
+        long.append(coord.lon)
+    rest_coords = (zip(lat,long))
+    for rest in rests:
+        rnames.append(rest.restaurant_name)
+    rest_dict = dict(zip(rnames,rest_coords))
+    return jsonify(rest_dict)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+# Close the session
+session.close()
